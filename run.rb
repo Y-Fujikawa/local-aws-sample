d5a15a6f48b9160d44f1a9f5527bc8198836c907
Yasuyuki Fujikawa <fujikawa0@gmail.com>
2014-07-13 21:00:58 +0900

    スコープを小さくする $#4

--- run.rb
+++ run.rb
@@ -6,30 +6,31 @@ require 'aws-sdk'
 Dotenv.load
 
 # DBに接続する
-conn = PG::connect(:host => ENV["PG_HOST"], :dbname => ENV["PG_DBNAME"], :port => ENV["PG_PORT"])
 begin
   # conn を使い PostgreSQL を操作する
+  conn = PG::connect(:host => ENV["PG_HOST"], :dbname => ENV["PG_DBNAME"], :port => ENV["PG_PORT"])
   p '接続できました'
   result = conn.exec("SELECT * FROM #{ENV['TARGET_TABLE']}")
-  # S3に保存するために体裁を整える
-  filename = ENV["CSV_FILE_NAME"] + Date.today.to_s + ".csv"
-  CSV.open(filename, "wb") do |csv|
-    result.values.each do |row|
-      csv << row
-    end
-  end
-
-  if ENV['USE_AWS'] == 'true'
-    # S3に接続してCSVファイルを保存する
-    s3 = AWS::S3.new(
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    )
-    # TODO S3上に同名のファイルが存在した場合、削除してからアップロードする？
-    s3.buckets[ENV['AWS_S3_BUCKET_NAME']].objects[File.basename(filename)].write(:file => File.expand_path(filename))
-  end
 ensure
   # データベースへのコネクションを切断する
   conn.finish
   p '切断しました'
+end
+
+# S3に保存するために体裁を整える
+filename = ENV["CSV_FILE_NAME"] + Date.today.to_s + ".csv"
+CSV.open(filename, "wb") do |csv|
+  result.values.each do |row|
+    csv << row
+  end
+end
+
+if ENV['USE_AWS'] == 'true'
+  # S3に接続してCSVファイルを保存する
+  s3 = AWS::S3.new(
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  )
+  # TODO S3上に同名のファイルが存在した場合、削除してからアップロードする？
+  s3.buckets[ENV['AWS_S3_BUCKET_NAME']].objects[File.basename(filename)].write(:file => File.expand_path(filename))
 end
