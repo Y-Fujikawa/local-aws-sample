4a831f4dbf0e1a45c78da3e5eca11e4438fe14bf
Y-Fujikawa <fujikawa0@gmail.com>
2015-11-23 18:21:17 +0900

    Use AWS Lambda Function from local

---
+++ exec_lambda.rb
@@ -0,0 +1,28 @@
+require 'dotenv'
+require 'aws-sdk'
+
+Dotenv.load
+
+lambda = Aws::Lambda::Client.new(
+  region: ENV['REGION'],
+		access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+		secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+)
+
+begin
+		param = { "key1": "Hello Ruby!!" }.to_json
+
+		resp = lambda.invoke({
+																									function_name: "hello-world",
+																									invocation_type: "Event",
+																									log_type: "None",
+																									client_context: "String",
+																									payload: param
+																							})
+
+		if resp.status_code == 202
+				p "接続できましたよ"
+		end
+rescue Aws::Lambda::Errors::ServiceError => e
+		p e.message
+end
